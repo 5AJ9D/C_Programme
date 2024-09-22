115a2f1cd4aaca37e2657b5709b9a6f8d033451e
5AJ9D <sajidhasan1299@gmail.com>
2024-09-22 22:54:50 +0600

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!"<<endl; //here its endl is breaking line.
+
+    string name1, name2, name3, name4;
+    int age1, age2, age3, age4, agen1,agen2;
+    string name;
+
+    cout<<"Input your name and age: ";
+    cin>>name1;cin>>age1;
+    cout<<"Input your name and age: ";
+    cin>>name2;cin>>age2;
+    cout<<"Input your name and age: ";
+    cin>>name3;cin>>age3;
+    cout<<"Input your name and age: ";
+    cin>>name4;cin>>age4;
+    cout<<" "<<endl;
+    cout<<"the First name is "<<name1<<" "<<"And his age is "<<age1<<endl;
+    cout<<"the Second name is "<<name2<<" "<<"And his age is "<<age2<<endl;
+    cout<<"the Third name is "<<name3<<" "<<"And his age is "<<age3<<endl;
+    cout<<"the Fourth name is "<<name4<<" "<<"And his age is "<<age4<<endl;
+    cout<<"Please enter";
+    cinn>>name;
+
+    return 0;
+}
